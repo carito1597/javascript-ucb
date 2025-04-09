6186eea00ac8dc662a618aa9e60b11cc80361808
Carolina Villalobos <carito15villalobos@gmail.com>
2025-04-08 20:09:26 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Obtener elementos del DOM
+const verDetallesButton = document.getElementById('verDetalles');
+const volverButton = document.getElementById('volver');
+const tarjeta = document.querySelector('.tarjeta');
+
+// Función para voltear la tarjeta
+
+// Función para volver a la cara frontal 
